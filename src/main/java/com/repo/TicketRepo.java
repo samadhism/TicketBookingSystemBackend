39a3dbcb3f2178a372c8970c1ce08ab865660bd3
samadhism <161122915+samadhism@users.noreply.github.com>
2024-12-09 01:37:51 +0530

    Implemented ticket controller and java classes for the backend

---
+++ src/main/java/com/repo/TicketRepo.java
@@ -0,0 +1,4 @@
+package com.repo;
+
+public class TicketRepo {
+}
---
+++ src/test/java/com/example/TicketBooking/TicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TicketBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
